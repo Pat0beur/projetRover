e84a5e791c66577e46ddbf7706f36821acc46e20
Pat0beur <julien@bougues.net>
2025-05-26 15:46:51 +0200

    Co-authored-by: mohawk2409 <mohawk2409@users.noreply.github.com>

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,3 +1,50 @@
+// package app;
+
+// import javafx.application.Application;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.stage.Stage;
+// import controllers.ControllerMenu;
+// import java.net.URL;
+
+// import java.io.IOException;
+
+// /**
+//  * JavaFX App
+//  */
+// public class App extends Application {
+
+//     private static Scene scene;
+
+    // @Override
+    // public void start(Stage stage) throws IOException {
+    //     scene = new Scene(loadFXML("secondary"), 640, 480);
+    //     stage.setScene(scene);
+    //     stage.show();
+    // }
+
+    // static void setRoot(String fxml) throws IOException {
+    //     scene.setRoot(loadFXML(fxml));
+    // }
+
+    // private static Parent loadFXML(String fxml) throws IOException {
+    //     FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    //     return fxmlLoader.load();
+    // }
+//     public void start(Stage primaryStage) throws Exception {
+//         FXMLLoader loader = new FXMLLoader();
+//         loader.setLocation(getClass().getResource("/app/Menu.fxml"));
+//         Parent root = loader.load();
+//         Scene scene = new Scene(root);
+//         primaryStage.setScene(scene) ;
+//         primaryStage.show() ;
+//     }
+//     public static void main(String[] args) {
+//         launch();
+//     }
+
+// }
 package app;
 
 import javafx.application.Application;
@@ -6,33 +53,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("FXML found: " + getClass().getResource("/app/Menu.fxml")); // test de debug
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/Menu.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Menu");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
---
+++ src/main/java/controllers/ControllerMenu.java
@@ -0,0 +1,5 @@
+package controllers;
+import javafx.application.Application;
+public class ControllerMenu {
+    
+}
