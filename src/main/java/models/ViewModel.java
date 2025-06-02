12784d7e9dc569e13814890284d9ce704a7d2ac1
mohawk2409 <raph.eleve@gmail.com>
2025-06-02 10:06:51 +0200

    init

--- src/main/java/models/ViewModel.java
+++ src/main/java/models/ViewModel.java
@@ -1,1 +1,11 @@
 package models;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class ViewModel {
+    public void initialize(){
+        System.out.println("ça marche");
+    }
+}
