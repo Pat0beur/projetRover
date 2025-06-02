2e42faf18f3ef0046e41cea8c7cd8a1ad621aa87
mohawk2409 <raph.eleve@gmail.com>
2025-06-02 09:57:59 +0200

    init controller

--- src/main/java/controllers/ControllerMenu.java
+++ src/main/java/controllers/ControllerMenu.java
@@ -1,5 +1,29 @@
 package controllers;
-import javafx.application.Application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
 public class ControllerMenu {
-    
+
+    @FXML
+    private Button btnJouer;
+    @FXML
+    private Button btnPersonnaliser;
+    @FXML
+    private Button btnCommandes;
+    @FXML
+    private Button btnQuitter;
+
+    @FXML
+    private RadioButton radiobtnEasy;
+    @FXML
+    private RadioButton radiobtnMedium;
+    @FXML
+    private RadioButton radiobtnHard;
+
+    @FXML
+    public void initialize() {
+        System.out.println("✅ Contrôleur initialisé !");
+    }
 }
