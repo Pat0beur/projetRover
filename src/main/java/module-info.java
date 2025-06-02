a4a6cfcec5a573cd53eaf46fd52eed43874a420c
mohawk2409 <raph.eleve@gmail.com>
2025-06-02 10:03:10 +0200

    caca

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module app {
     requires javafx.controls;
     requires javafx.fxml;
     opens controllers to javafx.fxml;
+    exports controllers;
     opens models to javafx.fxml;
+    exports models;
     opens views to javafx.fxml;
+    exports views;
     opens app to javafx.fxml;
     exports app;
 }
