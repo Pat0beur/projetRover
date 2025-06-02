cd9a6f60a4af87c79acdc0abfd898a9d0127117a
mohawk2409 <raph.eleve@gmail.com>
2025-06-02 11:23:00 +0200

    ControllerPersonnalisation

---
+++ src/main/java/controllers/ControllerPersonnalisation.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerPersonnalisation{
+    @FXML
+    private Button btnDroite;
+    @FXML
+    private Button btnGauche;
+    @FXML
+    private Button labelPersonnalisation;
+    @FXML
+    public void initialize(){
+        
+    }
+}
